eb06fa5de66af834ac6c8ae195c7fa2ea51837e7
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-02-16 14:18:41 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -23,8 +23,11 @@ const App = () => {
       <header className="App-header">
         <h1> My ToDo App </h1>
       </header>
-      < Form newTodo={newTodo} todos={todos} setTodos={setTodos} setNewTodo = {setNewTodo} />
-      < ToDoList todos={todos} setTodos={setTodos}/>
+      <div className="content"> 
+        < Form newTodo={newTodo} todos={todos} setTodos={setTodos} setNewTodo = {setNewTodo} />
+        < ToDoList todos={todos} setTodos={setTodos}/>
+      </div>
+      <footer className="footer" > {todos.filter(todo => !todo.completed).length} ToDo's left to do! </footer>
     </div>
   );
 }

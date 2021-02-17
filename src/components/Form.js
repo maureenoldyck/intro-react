83039cd86380a9ee8e26569633c7475351bace48
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-02-17 09:09:51 +0100

    Use useRef

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 const Form = ({setNewTodo, newTodo, todos, setTodos}) => {
 
-    const newTodoHandler = (e) => {
-        setNewTodo(e.target.value);
+    const inputRef = useRef();
+
+    const newTodoHandler = () => {
+        const inputElement = inputRef.current;
+        setNewTodo(inputElement);
     };
 
     const addTodo = (e) => {
@@ -21,7 +24,7 @@ const Form = ({setNewTodo, newTodo, todos, setTodos}) => {
     return (
         <div className="newToDo">
             <form> 
-                <input onChange={newTodoHandler} type="text" placeholder="Your ToDo.." value={newTodo} /> 
+                <input onChange={newTodoHandler} type="text" placeholder="Your ToDo.." value={newTodo} ref={inputRef} /> 
                 <button onClick={addTodo} > Add ToDo</button>
             </form>
       </div>
